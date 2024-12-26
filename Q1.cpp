959be42c78fcc5fa970934706bbbb506c7ed0ec3
ShreyashCode001 <shreyashcode001@gmail.com>
2024-12-26 18:51:55 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,47 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int amount;
-    cout<<"Enter Your Amount : ";
-    cin>>amount;
-    
+    cout << "Enter Your Amount: ";
+    cin >> amount;
 
-    if(amount%500==0){
-        cout<<(amount/500)<<" notes of 500"<<endl;
+    if (amount <= 0) {
+        cout << "Invalid amount. Please enter a positive value." << endl;
+        return 0;
     }
-    else if(amount%500!=0){
-        cout<<(amount/500)<<" notes of 500"<<endl;
-        if((amount%500)%100==0){
-            cout<<(amount%500)/100<<" notes of 100"<<endl;
-        }
-        else if(((amount%500)%100!=0)){
-            cout<<(amount%500)/100<<" notes of 100"<<endl;
-            if(((amount%500)%100)%50==0){
-                cout<<((amount%500)%100)/50<<" notes of 50"<<endl;
-            }
-            else if(((amount%500)%100)%50!=0){
-                cout<<((amount%500)%100)/50<<" notes of 50"<<endl;
-                if((((amount%500)%100)%50)%10==0){
-                    cout<<(((amount%500)%100)%50)/10<<" notes of 10"<<endl;
-                }
-                else if((((amount%500)%100)%50)%10!=0){
-                    cout<<(((amount%500)%100)%50)/10<<" notes of 10"<<endl;
-                    if(((((amount%500)%100)%50)%10)%5==0){
-                        cout<<((((amount%500)%100)%50)%10)/5<<" coins of 5"<<endl;
-                    }
-                    else if(((((amount%500)%100)%50)%10)%5!=0){
-                        cout<<((((amount%500)%100)%50)%10)/5<<" coins of 5"<<endl;
-                        if((((((amount%500)%100)%50)%10)%5)%1==0){
-                            cout<<(((((amount%500)%100)%50)%10)%5)/1<<" coins of 1"<<endl;
-                        }
-                    }
-                }
-            }
 
-        }
-    }
-    
+    int notes_500 = amount / 500;
+    amount %= 500;
+
+    int notes_100 = amount / 100;
+    amount %= 100;
+
+    int notes_50 = amount / 50;
+    amount %= 50;
+
+    int notes_10 = amount / 10;
+    amount %= 10;
+
+    int coins_5 = amount / 5;
+    amount %= 5;
+
+    int coins_1 = amount;
+
+    if (notes_500 > 0)
+        cout << notes_500 << " notes of 500" << endl;
+    if (notes_100 > 0)
+        cout << notes_100 << " notes of 100" << endl;
+    if (notes_50 > 0)
+        cout << notes_50 << " notes of 50" << endl;
+    if (notes_10 > 0)
+        cout << notes_10 << " notes of 10" << endl;
+    if (coins_5 > 0)
+        cout << coins_5 << " coins of 5" << endl;
+    if (coins_1 > 0)
+        cout << coins_1 << " coins of 1" << endl;
+
+    return 0;
 }
-
-
-
