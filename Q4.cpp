83fdedf2518dfd72a822be9561c170b68d4396ea
ShreyashCode001 <shreyashcode001@gmail.com>
2024-12-26 18:33:42 +0530

    Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 //WAP for finding the volume of the cylinder by taking radius and height as input.
 int main() {
-    double radius, height, volume;
-    cin >> radius >> height;
+    float radius, height, volume;
+    cout<<"Enter Radius of cylinder : ";
+    cin >> radius;
+    cout<<"Enter Height of cylinder : ";
+    cin>> height; 
     volume = 3.14 * radius * radius * height;
     cout << "Volume of the cylinder: " << volume << endl;
 }
